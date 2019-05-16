ff8a12a00d95058260bdec6b80eb3b01669e2505
SandraGro <sandra.guerreroflores@gmail.com>
2019-05-16 12:23:09 -0500

    se agregra middleware

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require ('path');
+const path = require('path');
 const app = express();
 const port = 3000;
 app.listen(port, () => console.log('server running'))
@@ -14,14 +14,31 @@ app.all('/bye', (request, response) => {
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/saludo', (request,response) =>{
+app.get('/saludo', (request, response) => {
     response.status(200).send("Hola, Sandra!");
 });
 
-app.post('/despedida', (request,response) =>{
+app.post('/despedida', (request, response) => {
     response.status(201).send("Adiós, Sandra!");
 });
 
-app.delete('/borrar', (request,response) =>{
+app.delete('/borrar', (request, response) => {
     response.status(401).send("Se borró");
+});
+
+app.all('*', (request, response, next) => {
+    console.log("El usuario hizo petición a: ", request.path);
+    next();
+});
+
+app.get('/sandra', (request, response) => {
+    response.send("Sandra Alejandra");
+});
+
+app.get('/sandra', (request, response) => {
+    response.send("Sandra Alejandra Guerrero Flores");
+});
+
+app.get('/usuario', (request, response) => {
+    response.send("Usuario: Sandra Alejandra Guerrero Flores");
 });
